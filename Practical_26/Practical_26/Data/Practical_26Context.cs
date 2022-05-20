9a09588c2cd1e21aff22c4e2f13bc130db12fc5c
Janvi Khatrani <khatranijanvi3@gmail.com>
2022-05-20 14:27:49 +0530

    Added .net 6 practical

---
+++ Practical_26/Practical_26/Data/Practical_26Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Practical_26.Models;
+
+namespace Practical_26.Data
+{
+    public class Practical_26Context : DbContext
+    {
+        public Practical_26Context (DbContextOptions<Practical_26Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Practical_26.Models.User>? User { get; set; }
+    }
+}
---
+++ Practical_26/Practical_26/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace Practical_26.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public int Age { get; set; }
+        public string? City { get; set; }
+
+    }
+}
